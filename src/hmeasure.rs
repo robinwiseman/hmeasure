c93ee294cefeadfcc21da0fa6b78ee6e76fafb80
Robin Wiseman <robin.wiseman@googlemail.com>
2023-04-11 20:08:32 +0100

    build out h-measure type + impls

--- src/hmeasure.rs
+++ src/hmeasure.rs
@@ -1,5 +1,6 @@
 use statrs::distribution::{Beta, Continuous, ContinuousCDF};
 use array2d::Array2D;
+use quadrature::double_exponential;
 
 use crate::datagen;
 use datagen::{BetaParams, BinaryClassScores};
@@ -22,9 +23,7 @@ impl CostRatioDensity {
     pub fn u1mc(&self, cost: f64) -> f64 {
         (1.0-cost)*self.c_density_obj.pdf(cost)
     }
-    pub fn cdf(&self, cost: f64) -> f64 {
-        self.c_density_obj.cdf(cost)
-    }
+    pub fn cdf(&self, cost: f64) -> f64 {self.c_density_obj.cdf(cost) }
     pub fn pdf(&self, cost: f64) -> f64 {
         self.c_density_obj.pdf(cost)
     }
@@ -41,25 +40,29 @@ pub struct HMeasure{
     pub h: Option<f64>
 }
 
+#[derive(Debug)]
+pub struct HMeasureResults{
+    pub h: f64,
+    pub convex_hull: Array2D<f64>
+}
+
 impl HMeasure{
     pub fn new(cost_distribution: CostRatioDensity, class0_prior: Option<f64>, class1_prior: Option<f64>) -> HMeasure {
-        let mut mh = HMeasure {
+        HMeasure {
             cost_distribution: cost_distribution,
             class0_prior: class0_prior,
             class1_prior: class1_prior,
             c0_num: None,
             c1_num: None,
             h: None
-        };
-
-        mh
+        }
     }
 
-    pub fn h_measure(&mut self, scores: BinaryClassScores) -> f64 {
+    pub fn h_measure(&mut self, scores: BinaryClassScores) -> HMeasureResults {
         let mut c0_scores = scores.class0;
         let mut c1_scores = scores.class1;
         self.c0_num = Some(c0_scores.len());
-        self.c1_num = Some(c0_scores.len());
+        self.c1_num = Some(c1_scores.len());
         let num_scores = self.c0_num.unwrap_or(0)+self.c1_num.unwrap_or(0);
         if self.class0_prior.is_none() || self.class1_prior.is_none() {
             self.class0_prior = Some(self.c0_num.unwrap_or(0) as f64/num_scores as f64);
@@ -67,12 +70,13 @@ impl HMeasure{
         }
         c0_scores.sort_by(|a, b| a.partial_cmp(b).unwrap());
         c1_scores.sort_by(|a, b| a.partial_cmp(b).unwrap());
-        if self.h.is_none(){
-            let (c_scores, c_classes) = self.merge_scores(c0_scores, c1_scores);
-            let roc_curve = self.build_roc(&c_scores, &c_classes);
-        }
+        let (c_scores, c_classes) = self.merge_scores(c0_scores, c1_scores);
+        let roc_curve = self.build_roc(&c_scores, &c_classes);
+        let (convex_hull, int_components) = self.build_chull(&roc_curve);
+        self.h = self._build_h(&int_components);
 
-        1.0
+        let h = self.h.unwrap();
+        HMeasureResults{ h, convex_hull }
     }
 
     fn merge_scores(&self, c0: Vec<f64>, c1: Vec<f64>) -> (Vec<f64>, Vec<u8>) {
@@ -148,6 +152,99 @@ impl HMeasure{
         }
 
         (dup_class_0.len(), dup_class_1.len())
+    }
+
+    fn build_chull(&self, roc_c: &Array2D<f64>) -> (Array2D<f64>, Vec<Vec<f64>>) {
+        let num_rows = roc_c.column_len();
+        let num_cols = roc_c.row_len();
+        let mut chull = Array2D::filled_with(0.0, num_rows, num_cols);
+        let mut int_components: Vec<Vec<f64>> = vec![];
+        let mut cval_loc_prior = [0.0, 0.0];
+        let mut cval_prior = 0.0;
+        let mut i = 0;
+        while i < num_rows-1 {
+            let cvals = self._cvals(&roc_c, i);
+            let c_argmin = HMeasure::argmin(&cvals);
+            let cval_loc = [roc_c[(i+c_argmin+1,0)],roc_c[(i+c_argmin+1,1)]];
+            chull[(i+1,0)] = cval_loc[0];
+            chull[(i+1,1)] = cval_loc[1];
+            int_components.push(vec![cvals[c_argmin], cval_prior, cval_loc_prior[0], cval_loc_prior[1]]);
+            cval_loc_prior = cval_loc;
+            cval_prior = cvals[c_argmin];
+            i += c_argmin+1;
+        }
+
+        (chull, int_components)
+    }
+
+    fn _cvals(&self, roc_c: &Array2D<f64>, current_idx: usize) -> Vec<f64> {
+        let mut cvals: Vec<f64> = vec![];
+        let current_point: [f64;2] = [roc_c[(current_idx,0)],roc_c[(current_idx,1)]];
+        let mut i_point_idx = current_idx + 1;
+        while i_point_idx < roc_c.column_len() {
+            let i_point: [f64;2] = [roc_c[(i_point_idx,0)],roc_c[(i_point_idx,1)]];
+            cvals.push(self._cval(&current_point, &i_point));
+            i_point_idx += 1;
+        }
+        cvals
+    }
+
+    fn _cval(&self, current_point: &[f64; 2], i_point: &[f64;2]) -> f64 {
+        let numerator = self.class1_prior.unwrap_or(0.5)*(i_point[0]-current_point[0]);
+        let denominator = self.class0_prior.unwrap_or(0.5)*(i_point[1]-current_point[1]) + self.class1_prior.unwrap_or(0.5)*(i_point[0]-current_point[0]);
+        numerator/denominator
+    }
+
+    fn argmin(v: &Vec<f64>) -> usize {
+        let mut min_idx = 0;
+        let mut min_val = v[0];
+        for (i, &val) in v.iter().enumerate() {
+            if val < min_val {
+                min_idx = i;
+                min_val = val;
+            }
+        }
+        min_idx
+    }
+
+    fn _build_h(&self, int_components: &Vec<Vec<f64>>) -> Option<f64> {
+        let l = self._build_l(&int_components).unwrap();
+        let l_max = self._l_max().unwrap();
+        Some(1.0-l/l_max)
+    }
+
+    fn _build_l(&self, int_components: &Vec<Vec<f64>>) -> Option<f64> {
+        let mut l = 0.0;
+        let mut i = 0;
+        while i < int_components.len() {
+            let int_vals = &int_components[i];
+            l += self._int_l_step(int_vals);
+            i += 1
+        }
+        Some(l)
+    }
+
+    fn _int_l_step(&self, int_vals: &Vec<f64>) -> f64 {
+        let r0i = int_vals[3];
+        let r1i = int_vals[2];
+        let ci = int_vals[1];
+        let cip1 = int_vals[0];
+        let c0_prior = self.class0_prior.unwrap();
+        let c1_prior = self.class1_prior.unwrap();
+        let int_uc = double_exponential::integrate(|x| self.cost_distribution.uc(x), ci, cip1, 1e-9).integral;
+        let int_u1mc = double_exponential::integrate(|x| self.cost_distribution.u1mc(x), ci, cip1, 1e-9).integral;
+
+        let l_step = c0_prior*(1.0-r0i)*int_uc + c1_prior*r1i*int_u1mc;
+
+        return l_step
+    }
+
+    fn _l_max(&self) -> Option<f64> {
+        let c0_prior = self.class0_prior.unwrap();
+        let c1_prior = self.class1_prior.unwrap();
+        let mut l_max = double_exponential::integrate(|x| self.cost_distribution.uc(x), 0.0, c1_prior, 1e-9).integral*c0_prior;
+        l_max += double_exponential::integrate(|x| self.cost_distribution.u1mc(x), c1_prior, 1.0,1e-9).integral*c1_prior;
+        Some(l_max)
     }
 }
 
